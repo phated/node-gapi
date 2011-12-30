3e427a648d79ba4373f1f07e5d0fc61b488985f7
Blaine Bublitz <phate666x@gmail.com>
2011-12-29 18:51:10 -0700

    added Express example

---
+++ examples/express-example/routes/comments.js
@@ -0,0 +1,14 @@
+var gapi = require('gapi');
+
+exports.comments = function(req, res){
+	gapi.server.load('plus','v1',function() {
+		var commentsRequest = gapi.server.plus.comments.list({activityId: req.params.id});
+		commentsRequest.execute(function(resp) {
+			if (req.xhr) {
+				res.partial('comments', resp);
+			} else {
+				res.render('comments', resp);
+			}
+		});
+	});
+});
